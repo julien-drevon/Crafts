a3d21cc94e19e99b9743a36945f30aa5b08a208a
worm-rock <drevon.julien@gmail.com>
2023-07-12 13:58:25 +0200

    add php project config

---
+++ Php/Crafts.Base/src/tests/HexaArchitecture/ClassiqueHexaSampleChicagoStyleTest.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ClassiqueHexaSampleChicagoStyleTest extends TestCase
+{
+    public function test_idiot()
+    {
+        $attempt = false;
+        $this->assertEquals(false,$attempt);
+    }
+}
