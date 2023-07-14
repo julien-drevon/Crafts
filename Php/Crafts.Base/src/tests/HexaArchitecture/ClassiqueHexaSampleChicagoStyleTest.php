1d58cabbd99b576ca1df07426d2d6c63199b4fb4
worm-rock <drevon.julien@gmail.com>
2023-07-14 14:57:57 +0200

    Update ClassiqueHexaSampleChicagoStyleTest.php

--- Php/Crafts.Base/src/tests/HexaArchitecture/ClassiqueHexaSampleChicagoStyleTest.php
+++ Php/Crafts.Base/src/tests/HexaArchitecture/ClassiqueHexaSampleChicagoStyleTest.php
@@ -4,9 +4,39 @@ use PHPUnit\Framework\TestCase;
 
 class ClassiqueHexaSampleChicagoStyleTest extends TestCase
 {
-    public function test_idiot()
+    public function test_GivenAnExemplePortMethod_WhenICallHer_IGotAResponse()
     {
-        $attempt = false;
-        $this->assertEquals(false,$attempt);
+        $myRequest = new PrimaryPortRequestExemple();
+        $givenExemplePort= new PrimaryAdapterExemple();
+        $attempt=  $givenExemplePort->callMe($myRequest);
+        
+        
+        $this->assertEquals(true, (new PrimaryPortResponse(true))->getIsOk());
+    }
+}
+
+class PrimaryAdapterExemple
+{
+    public function callMe(PrimaryPortRequestExemple $request) : PrimaryPortResponse
+    {
+        return new PrimaryPortResponse(true);
+    }
+    
+}
+class PrimaryPortRequestExemple
+{}
+class PrimaryPortResponse
+{
+    
+    protected bool $isOk;
+
+    public function __construct(bool $isOk = false)
+    {
+        $this->isOk = $isOk;
+    }
+
+    public function getIsOk(): bool
+    {
+        return $this->isOk;
     }
 }
