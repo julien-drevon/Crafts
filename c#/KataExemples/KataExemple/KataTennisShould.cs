8150023bab50b44bbae7e393250a0ea651a2b2a2
julienD <julien.drevon@octo.com>
2023-07-19 19:58:39 +0200

    Update KataTennisShould.cs

--- c#/KataExemples/KataExemple/KataTennisShould.cs
+++ c#/KataExemples/KataExemple/KataTennisShould.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
-using FluentAssertions.Execution;
+﻿using FluentAssertions;
 
 namespace KataExemples;
 
@@ -14,10 +8,7 @@ public class KataTennisShould
     public void StartGame_0_0()
     {
         var givenGame = new Game();
-        givenGame.Points.Should().BeEquivalentTo(new[]
-        {
-            "0","0"
-        });
+        givenGame.Points.Should().BeEquivalentTo("0 0");
     }
 
     [Fact]
@@ -25,22 +16,54 @@ public class KataTennisShould
     {
         var givenGame = new Game();
         givenGame.SetPoint(JoueurNumber.J1);
-        givenGame.Points.Should().BeEquivalentTo(new[]
-        {
-            "15","0"
-        });
+        givenGame.Points.Should().BeEquivalentTo("15 0");
     }
-
 
     [Fact]
     public void SecondPoint_15_15()
     {
         var givenGame = new Game();
+        givenGame.SetPoint(JoueurNumber.J1);
         givenGame.SetPoint(JoueurNumber.J2);
-        givenGame.Points.Should().BeEquivalentTo(new[]
-        {
-            "0","15"
-        });
+        givenGame.Points.Should().BeEquivalentTo("15 15");
+    }
+
+    [Fact]
+    public void ThirdPoint_30_15()
+    {
+        var givenGame = new Game();
+
+        givenGame.SetPoint(JoueurNumber.J1);
+        givenGame.SetPoint(JoueurNumber.J2);
+        givenGame.SetPoint(JoueurNumber.J1);
+
+        givenGame.Points.Should().BeEquivalentTo("30 15");
+    }
+
+    [Fact]
+    public void FourthPoint_40_15()
+    {
+        var givenGame = new Game();
+
+        givenGame.SetPoint(JoueurNumber.J1);
+        givenGame.SetPoint(JoueurNumber.J2);
+        givenGame.SetPoint(JoueurNumber.J1);
+        givenGame.SetPoint(JoueurNumber.J1);
+
+        givenGame.Points.Should().Be("40 15");
+    }
+
+    [Fact]
+    public void Players2_Egalizet_40_40_Egalité()
+    {
+        var givenGame = new Game();
+
+        givenGame.SetPoint(JoueurNumber.J1);
+        givenGame.SetPoint(JoueurNumber.J2);
+        givenGame.SetPoint(JoueurNumber.J1);
+        givenGame.SetPoint(JoueurNumber.J1);
+
+        givenGame.Points.Should().BeEquivalentTo("40 15");
     }
 }
 
@@ -58,10 +81,11 @@ public class Game
 
     private int[] _PlayersPoints = new int[2];
 
-    public object Points { get =>new []{ _PlayersPoints[0].ToString(), _PlayersPoints[1].ToString() }; }
+    public object Points { get => $"{_PlayersPoints[0].ToString()} {_PlayersPoints[1].ToString()}"; }
 
     internal void SetPoint(JoueurNumber wichPlayer)
     {
-        _PlayersPoints[(int)wichPlayer] = 15;
+        var increment = _PlayersPoints[(int)wichPlayer] < 30 ? 15 : 10;
+        _PlayersPoints[(int)wichPlayer] += increment;
     }
 }
