980be2ae581de6dffc3dc6abceac67461faa92b5
worm-rock <drevon.julien@gmail.com>
2023-07-20 01:02:47 +0200

    add librairie project

---
+++ c#/KataExemples/KataExemple/FizzBuzzUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace Kata
+{
+    public static class FizzBuzzUtils
+    {
+        public static string FizzBuzz(int value)
+        {
+
+            if (value % 15 == 0) return "FizzBuzz";
+            if (value % 5 == 0) return "Buzz";
+            if (value % 3 == 0) return "Fizz";
+
+            return value.ToString();
+        }
+    }
+}
