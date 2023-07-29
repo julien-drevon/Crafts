2ea3151fe7ad64a51ebb1bed4dfd803b96d6f040
worm-rock <drevon.julien@gmail.com>
2023-07-29 16:30:31 +0200

    renaming

--- c#/GildedRose/src/Refacto/GuildedRose.RefactoConsole/Program.cs
+++ c#/GildedRose/src/Refacto/GuildedRose.RefactoConsole/Program.cs
@@ -8,13 +8,13 @@ var app = new Program();
 
 var itemems = new[]
 {
-            CreateItemFactory.Create(ItemsName.Plus5DexterityVest, 10, 20),
-            CreateItemFactory.Create(ItemsName.AgedBrie, 2, 0),
-            CreateItemFactory.Create(ItemsName.ElixirOfMangoust, 5, 7),
-            CreateItemFactory.Create(ItemsName.Sulfuras, 0, 80),
-            CreateItemFactory.Create(ItemsName.BackstagePassesTAFKAL80ETCConcert, 15, 20),
-            CreateItemFactory.Create(ItemsName.ConjuredManaCake, 3, 6),
-        };
+    CreateItemFactory.Create(ItemsName.Plus5DexterityVest, sellIn: 10, quality: 20),
+    CreateItemFactory.Create(ItemsName.AgedBrie, sellIn: 2, quality: 0),
+    CreateItemFactory.Create(ItemsName.ElixirOfMangoust, sellIn: 5, quality: 7),
+    CreateItemFactory.Create(ItemsName.Sulfuras, sellIn: 0, quality: 80),
+    CreateItemFactory.Create(ItemsName.BackstagePassesTAFKAL80ETCConcert, sellIn: 15, quality: 20),
+    CreateItemFactory.Create(ItemsName.ConjuredManaCake, sellIn: 3, quality: 6),
+};
 
 var guildedRose = new GuildeadRose(itemems);
 
