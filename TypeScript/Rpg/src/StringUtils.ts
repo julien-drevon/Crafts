73c20679585a5ee0848a146a0211b8b1ce7a5f65
worm-rock <drevon.julien@gmail.com>
2024-04-17 13:20:32 +0200

    Update StringUtils.ts

--- TypeScript/Rpg/src/StringUtils.ts
+++ TypeScript/Rpg/src/StringUtils.ts
@@ -1,24 +1,26 @@
 export function isNullOrEmpty(val: string): boolean {
-  return !val || val.isNullOrEmpty();
+  return !val || val.isEmpty();
 }
+
 export function isNotNullOrEmpty(val: string): boolean {
-  if (val && val.isNotNullOrEmpty()) {
+  if (val && val.isNotEmpty()) {
     return true;
   }
 
   return false;
 }
+
 declare global {
   export interface String {
-    isNullOrEmpty(): boolean;
-    isNotNullOrEmpty(): boolean;
+    isEmpty(): boolean;
+    isNotEmpty(): boolean;
   }
 }
 
-String.prototype.isNullOrEmpty = function () {
+String.prototype.isEmpty = function () {
   return !this || this === "";
 };
 
-String.prototype.isNotNullOrEmpty = function () {
-  return !this.isNullOrEmpty();
+String.prototype.isNotEmpty = function () {
+  return !this.isEmpty();
 };
