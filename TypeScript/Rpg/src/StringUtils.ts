f4f818cd341b96f4949a7402085d9e2b8686bcf4
worm-rock <drevon.julien@gmail.com>
2024-04-17 13:19:26 +0200

    add string extension methodes

--- TypeScript/Rpg/src/StringUtils.ts
+++ TypeScript/Rpg/src/StringUtils.ts
@@ -1,6 +1,24 @@
 export function isNullOrEmpty(val: string): boolean {
-  return !val || val === "";
+  return !val || val.isNullOrEmpty();
 }
 export function isNotNullOrEmpty(val: string): boolean {
-  return !isNullOrEmpty(val);
+  if (val && val.isNotNullOrEmpty()) {
+    return true;
+  }
+
+  return false;
 }
+declare global {
+  export interface String {
+    isNullOrEmpty(): boolean;
+    isNotNullOrEmpty(): boolean;
+  }
+}
+
+String.prototype.isNullOrEmpty = function () {
+  return !this || this === "";
+};
+
+String.prototype.isNotNullOrEmpty = function () {
+  return !this.isNullOrEmpty();
+};
